733f51fd76ced31a99fc24e60f3ae50dff50a99e
Elycin <elycitpical@hotmail.com>
2016-05-25 00:24:36 -0400

    Newtonsoft Checkign

--- pitter-2/Init/Runtime.vb
+++ pitter-2/Init/Runtime.vb
@@ -4,6 +4,7 @@ Public Class Runtime
     Public working_directory = GetFolderPath(SpecialFolder.ApplicationData) + "\Pitter\"
     Private Sub Runtime_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         dircheck()
+        newtonsoftcheck()
         processcheck()
         decision()
         Me.Close()
@@ -37,6 +38,24 @@ Public Class Runtime
     Public Sub dircheck()
         If My.Computer.FileSystem.DirectoryExists(working_directory) = False Then
             MkDir(working_directory)
+        End If
+    End Sub
+    Public Sub newtonsoftcheck()
+        If My.Computer.FileSystem.FileExists(working_directory + "Newtonsoft.Json.dll") Then
+            'Check if it exists in current dir
+            If My.Computer.FileSystem.FileExists(Environment.CurrentDirectory + "\Newtonsoft.Json.dll") = False Then
+                'Copy
+                My.Computer.FileSystem.CopyFile(working_directory + "Newtonsoft.Json.dll", Environment.CurrentDirectory + "\Newtonsoft.Json.dll")
+                'Hide
+                File.SetAttributes(Environment.CurrentDirectory + "\Newtonsoft.Json.dll", FileAttributes.Hidden)
+            End If
+        Else
+            Dim client As New Net.WebClient
+            client.DownloadFile("http://download.pitter.us/Newtonsoft.Json.dll", working_directory + "Newtonsoft.Json.dll")
+            'Copy
+            My.Computer.FileSystem.CopyFile(working_directory + "Newtonsoft.Json.dll", Environment.CurrentDirectory + "\Newtonsoft.Json.dll")
+            'Hide
+            File.SetAttributes(Environment.CurrentDirectory + "\Newtonsoft.Json.dll", FileAttributes.Hidden)
         End If
     End Sub
 End Class
