e5c056a016d2adbf2fdd0c08bd25d74b3bdd8435
Elycin <elycitpical@hotmail.com>
2016-07-14 13:55:30 -0400

    inherit DPI from OS

--- pitter-2/App-Web/WebApp.Designer.vb
+++ pitter-2/App-Web/WebApp.Designer.vb
@@ -185,8 +185,8 @@ Partial Class WebApp
         '
         'WebApp
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.ClientSize = New System.Drawing.Size(935, 523)
         Me.Controls.Add(Me.WebBrowser1)
         Me.Name = "WebApp"
