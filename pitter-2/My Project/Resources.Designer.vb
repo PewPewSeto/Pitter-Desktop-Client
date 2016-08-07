d977c63de7f5ff021d43fbca374e8c3063866af3
Elycin <elycitpical@hotmail.com>
2016-08-07 01:15:02 -0400

    Removing single isntance ability to remove dialog.

--- pitter-2/My Project/Resources.Designer.vb
+++ pitter-2/My Project/Resources.Designer.vb
@@ -71,16 +71,6 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property bg_2() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("bg-2", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
         '''  Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         '''</summary>
         Friend ReadOnly Property complete() As System.IO.UnmanagedMemoryStream
